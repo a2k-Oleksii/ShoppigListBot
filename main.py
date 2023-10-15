c727bee8a22c7b49264f6a89cd1d62fb52f4a1a1
a2k-Oleksii <a2k-oleksii@ukr.net>
2023-10-15 20:26:01 +0300

    Add webhook on ngrok

--- main.py
+++ main.py
@@ -1,13 +1,42 @@
+
 import telebot
+import flask
+import time
 
 API_TOKEN = "6454697652:AAEBf2IBBK4NUsJ0jqtyHW4tyHMPthGZvhY"
 shop_bot = telebot.TeleBot(API_TOKEN)
+
+app = flask.Flask(__name__)
+
+WEBHOOK_HOST = "2f4a-178-214-192-176.ngrok-free.app"
+WEBHOOK_PORT = 8448
+WEBHOOK_LISTEN = "0.0.0.0"
+
+WEBHOOK_URL_BASE = "https://%s" % WEBHOOK_HOST
+WEBHOOK_URL_PATH = "/%s/" % API_TOKEN
 
 commands = ["Add to list", "Get from list", "Delete from list"]
 keyboard = telebot.types.ReplyKeyboardMarkup()
 keyboard.row(*commands)
 
 user_data = {}
+
+
+@app.route("/", methods=["GET", "HEAD"])
+def index():
+    print("index")
+    return "Shopping Bot"
+
+
+@app.route(WEBHOOK_URL_PATH, methods=["POST"])
+def webhook():
+    if flask.request.headers.get("content-type") == "application/json":
+        json_string = flask.request.get_data().decode("utf-8")
+        update = telebot.types.Update.de_json(json_string)
+        shop_bot.process_new_updates([update])
+        return ""
+    else:
+        flask.abort(403)
 
 
 @shop_bot.message_handler(commands=["start"])
@@ -21,7 +50,6 @@ def command(message):
         user_data[message.chat.id] = [[], None, None]
     if message.text == "Add to list":
         user_data[message.chat.id][1] = "add"
-        # current_operation = "add"
         shop_bot.reply_to(message, "You can add items to the list")
     elif message.text == "Get from list":
         user_data[message.chat.id][1] = "get"
@@ -53,4 +81,9 @@ def callback_handler(call):
     shop_bot.edit_message_reply_markup(call.message.chat.id, user_data[call.message.chat.id][2], reply_markup=inline_keyboard)
 
 
-shop_bot.polling()
+shop_bot.remove_webhook()
+time.sleep(0.1)
+
+shop_bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
+
+app.run(host="localhost", port=8448)
